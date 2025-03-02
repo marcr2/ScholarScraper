d5cf795da992271740145309e93a4ed65024b82b
cnantoninor <antonino.rau@gmail.com>
2025-03-02 17:21:25 -0500

    init refact

--- orchestrator.py
+++ orchestrator.py
@@ -6,11 +6,18 @@ from visualizer import plotData
 query = input("What is your query? ")
 max_results = int(input("How many articles would you like to fetch? "))
 processType = input("Will you be using LLM or NLP? ")
-field = input("What field is your research in? (Ie. molecular biology, machine learning) ")
-target_object = input("What would you like to group by? (Ie. treatment option, deep learning algorithm) ")
-fetch_semantic_scholar_abstracts(query, max_results) # scrape articles
-import_data = processWSData(processType) # set up for LLM/NLP
-processed_data = llmRead(import_data, field, target_object) # categorize articles using LLM
+field = input(
+    "What field is your research in? (Ie. molecular biology, machine learning)"
+)
+target_object = input(
+    "What would you like to group by? (Ie. treatment option, deep learning algorithm) "
+)
+
+fetch_semantic_scholar_abstracts(query, max_results)  # scrape articles
+import_data = processWSData(processType)  # set up for LLM/NLP
+processed_data = llmRead(
+    import_data, field, target_object
+)  # categorize articles using LLM
 scored_data = processLLMData(processed_data)
 print(scored_data.head())
-plotData(scored_data) # visualize data
+plotData(scored_data)  # visualize data
--- scraper.py
+++ scraper.py
@@ -1,24 +1,29 @@
-import requests
 import time
-import pandas as pd
-from tqdm import tqdm
-import json
 
-def processSearch(publication): # collect data from fetched publication
-  title = publication["title"]
-  abstract = publication["abstract"]
-  authors = publication["authors"]
-  pub_id = publication["paperId"]
-  num_citations = publication["citationCount"]
-  pub_year = publication["year"]
-  processed_pub = [title, authors, num_citations, pub_year, pub_id, abstract]
-  return processed_pub
+import pandas as pd
+import requests
+from tqdm import tqdm
+
+
+def processSearch(publication):  # collect data from fetched publication
+    title = publication["title"]
+    abstract = publication["abstract"]
+    authors = publication["authors"]
+    pub_id = publication["paperId"]
+    num_citations = publication["citationCount"]
+    pub_year = publication["year"]
+    processed_pub = [title, authors, num_citations, pub_year, pub_id, abstract]
+    return processed_pub
+
 
 def fetch_semantic_scholar_abstracts(query, max_results):
     base_url = "https://api.semanticscholar.org/graph/v1/paper/search"
     headers = {"Accept": "application/json"}
     rows = []
-    final_list = pd.DataFrame(rows, columns=["title", "authors", "num_citations", "pub_year", "pub_id", "abstract"])
+    final_list = pd.DataFrame(
+        rows,
+        columns=["title", "authors", "num_citations", "pub_year", "pub_id", "abstract"],
+    )
     offset = 0
     limit = 100
     papers_added = 0
@@ -28,40 +33,49 @@ def fetch_semantic_scholar_abstracts(query, max_results):
         total=max_results,
         desc="Fetching papers",
         bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}]",
-        colour="WHITE"
+        colour="WHITE",
     ) as pbar:
-        while len(final_list) < max_results: # send request for publications
+        while len(final_list) < max_results:  # send request for publications
             retries = 0
+            response = None
             while retries < max_retries:
                 params = {
                     "query": query,
                     "offset": offset,
                     "limit": limit,
-                    "fields": "title,authors,year,citationCount,paperId,abstract"
+                    "fields": "title,authors,year,citationCount,paperId,abstract",
                 }
-                response = requests.get(base_url, headers=headers, params=params)
+                response = requests.get(
+                    base_url, headers=headers, params=params, timeout=30
+                )
                 if response.status_code == 200:
                     break
                 else:
-                    print(f"Error: {response.status_code} - {response.text}. Retrying in {retry_wait_time} seconds...")
+                    print(
+                        f"Error: {response.status_code} - {response.text}. Retrying in {retry_wait_time} seconds..."
+                    )
                     retries += 1
                     time.sleep(retry_wait_time)
             if retries == max_retries:
                 print("Max retries reached. Exiting.")
                 break
+            if not response:
+                break
             data = response.json()
             if not data.get("data"):
                 break
-            for paper in data["data"]: # processes response and adds to pandas dataframe
+            for paper in data[
+                "data"
+            ]:  # processes response and adds to pandas dataframe
                 if paper.get("abstract"):
                     processed_result = processSearch(paper)
                     final_list.loc[len(final_list)] = processed_result
                     papers_added += 1
-                    final_list.to_csv('output.csv', index=False)
+                    final_list.to_csv("output.csv", index=False)
             pbar.update(papers_added)
             papers_added = 0
             offset += len(data["data"])
-            if len(final_list) < max_results: # only waits if needed
+            if len(final_list) < max_results:  # only waits if needed
                 time.sleep(300)  # rate limit is 100 requests per 5 minutes
             else:
                 print(f"Caught {len(final_list)} articles!")
